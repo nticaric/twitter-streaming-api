71c1e30cde88e54f4beb8aacd676e411369bda5d
Nenad Ticaric <nticaric@gmail.com>
2014-10-18 00:36:00 +0200

    retry

--- src/TwitterStream.php
+++ src/TwitterStream.php
@@ -4,38 +4,52 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
 use GuzzleHttp\Stream\Stream;
 use GuzzleHttp\Stream\Utils;
+use GuzzleHttp\Event\AbstractTransferEvent;
+use GuzzleHttp\Subscriber\Retry\RetrySubscriber;
 
 class TwitterStream {
 
-	private $endpoint = "https://stream.twitter.com/1.1/";
-	
-	public function __construct($config) {
+    private $endpoint = "https://stream.twitter.com/1.1/";
+    private $retries  = 16;
+    
+    public function __construct($config) {
 
-		$this->client = new Client([
-			'base_url' => $this->endpoint,
-			'defaults' => ['auth' => 'oauth', 'stream' => true],
-		]);
-		$oauth = new Oauth1($config);
+        $this->client = new Client([
+            'base_url' => $this->endpoint,
+            'defaults' => ['auth' => 'oauth', 'stream' => true],
+        ]);
+        $oauth = new Oauth1($config);
 
-		$this->client->getEmitter()->attach($oauth);
-	}
+        $retry = new RetrySubscriber([
+            'filter' => RetrySubscriber::createStatusFilter([503]),
+            'max'    => $this->retries,
+        ]);
 
-	public function getStatuses($param, $callback)
-	{
-		$response = $this->client->post('statuses/filter.json', [
-		    'body'   => $param
-		]);
+        $this->client->getEmitter()->attach($retry);
+        $this->client->getEmitter()->attach($oauth);
+    }
 
-		$body = $response->getBody();
+    public function setRetries($num)
+    {
+        $this->retries = $num;
+    }
 
-		while (!$body->eof()) {
-		    $line = Utils::readLine($body);
+    public function getStatuses($param, $callback)
+    {
+        $response = $this->client->post('statuses/filter.json', [
+            'body'   => $param
+        ]);
+
+        $body = $response->getBody();
+
+        while (!$body->eof()) {
+            $line = Utils::readLine($body);
             $data = json_decode($line, true);
             if(is_null($data)) continue;
             call_user_func($callback, $data);
             if( ob_get_level() > 0 ) ob_flush();
-        	flush();
-		}
+            flush();
+        }
 
-	}
+    }
 }
