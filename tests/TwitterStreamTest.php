d8f5d48cfb9b117a8aa66859edb2c65f75cd5980
Nenad Tičarić <nticaric@gmail.com>
2015-03-27 23:26:28 +0100

    upgrade guzzle

--- tests/TwitterStreamTest.php
+++ tests/TwitterStreamTest.php
@@ -8,13 +8,15 @@ class TwitterStreamTest extends \PHPUnit_Framework_TestCase {
 	public function testOAuthConnection()
 	{
 		$stream = new TwitterStream(array(
-		    'consumer_key'    => 'my_key',
-		    'consumer_secret' => 'my_secret',
-		    'token'           => 'my_token',
-		    'token_secret'    => 'my_token_secret'
+			'consumer_key'    => 'my_key',
+			'consumer_secret' => 'my_secret',
+			'token'           => 'my_token',
+			'token_secret'    => 'my_token_secret'
 		));
 
-		$res = $stream->getUsers();
-		print_r($res);
+		$stream->getStatuses(
+            ['track' => 'car'], function($tweet) {
+                echo $tweet;
+        });
 	}
 }
