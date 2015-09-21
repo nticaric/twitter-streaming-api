589153fc25bf9ccf312527a41ec87b92801868d0
Nenad Ticaric <nticaric@gmail.com>
2015-09-21 11:18:17 +0200

    TwitterRestApi

---
+++ src/TwitterRestApi.php
@@ -0,0 +1,48 @@
+<?php namespace Nticaric\Twitter;
+use GuzzleHttp\Client;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+
+class TwitterRestApi
+{
+    private $endpoint = "https://api.twitter.com/1.1/";
+
+    /**
+     * If $postAsUser is set to true, the app will post as the user, else it will post as the platform
+     */
+    public function __construct($credentials)
+    {
+
+        $this->client = new Client([
+            'base_url' => $this->endpoint,
+            'defaults' => ['auth' => 'oauth'],
+        ]);
+
+        $oauth = new Oauth1($credentials);
+
+        $this->client->getEmitter()->attach($oauth);
+    }
+
+    public function getFriendsIds()
+    {
+        $response = $this->client->get('friends/ids.json')->getBody();
+        return json_decode($response, true);
+    }
+
+    public function getApplicationRateLimitStatus()
+    {
+        $response = $this->client->get('application/rate_limit_status.json')->getBody();
+        return json_decode($response, true);
+    }
+
+    public function postFavoritesCreate($postID, $includeEntities = false)
+    {
+        $response = $this->client->post('favorites/create.json', [
+            'body' => [
+                'id' => $postID,
+                'include_entities' => $includeEntities
+            ]
+        ])->getBody();
+        
+        return json_decode($response, true);
+    }
+}
